f0307127be2de9deaefadd190c32ebd8926155fc
ivanmendozav <ivanmendozav@yahoo.com>
2023-03-03 08:44:49 -0500

    Actualizado a presentación ante el centro de estadística el día 2 de febrero de 2023.

--- libraries.R
+++ libraries.R
@@ -10,7 +10,7 @@ connect_pgsql <- function(database="bici2023"){
 getData <- function(limit=100000){
   require("RPostgreSQL")
   con_t <- connect_pgsql("bici2023")
-  query <- paste("select * from locations where recorded_at between '2019-09-01' and '2020-01-01' limit ",limit, sep="")
+  query <- paste("select * from locations where recorded_at between '2019-10-01' and '2019-12-01' limit ",limit, sep="")
   df <- dbGetQuery(con_t, query)
   dbDisconnect(con_t)
   return(df)
@@ -19,6 +19,7 @@ getData <- function(limit=100000){
 segmentTrips <- function(subdata, minttime=300, maxtime=3600){
   library(lubridate)
   library(dplyr)
+  sumdistance <- 0
   #preprocessing
   subdata$activity_type <- factor(subdata$activity_type)
   subdata$device_model <- factor(subdata$device_model)
@@ -37,13 +38,21 @@ segmentTrips <- function(subdata, minttime=300, maxtime=3600){
       for (pointindex in 1:nrow(userpoints)){ #for each data point
         point <- userpoints[pointindex,]
         if (point$activity_type!="still"){ #merge moving points
+          if(nrow(movingpoints)>1){
+            last_point <- tail(movingpoints,1)
+            distance <- sqrt((point$x-last_point$x)^2+(point$y-last_point$y)^2)
+            sumdistance <- sumdistance + distance
+          }  
           movingpoints <- rbind(movingpoints,point)
         } else{ #when stop add to all trips array
           if(nrow(movingpoints)>1){ #only if there are enough points (At least origin and destination)
             #create trip object
-            trip <- data.frame(user=user, tripid = id, ox=next_origin$x, oy=next_origin$y, dx=point$x, dy=point$y, olong=next_origin$longitude, olat=next_origin$latitude, dlong=point$longitud, dlat=point$latitude, starttime=head(movingpoints,1)$recorded_at, endtime=tail(movingpoints,1)$recorded_at, mode=labelmode(movingpoints), points=nrow(movingpoints))
+            distance <- sqrt((point$x-last_point$x)^2+(point$y-last_point$y)^2)
+            sumdistance <- sumdistance + distance
+            trip <- data.frame(user=user, tripid = id, ox=next_origin$x, oy=next_origin$y, dx=point$x, dy=point$y, olong=next_origin$longitude, olat=next_origin$latitude, dlong=point$longitud, dlat=point$latitude, starttime=head(movingpoints,1)$recorded_at, endtime=tail(movingpoints,1)$recorded_at, mode=labelmode(movingpoints), points=nrow(movingpoints), tdistance=sumdistance)
             allusertrips <- rbind(allusertrips,trip)
             movingpoints <- data.frame() #new trip
+            sumdistance <- 0
             id <- id + 1 #new trip id
             next_origin = point #next origin is previous destination
           }
@@ -87,20 +96,20 @@ getAttractors <- function(tripdata, eps=25, minPts=10){
   centroids <- as.data.frame(centroids)
   print(centroids)
   wgscent <- changecoordsystem(centroids,longlabel = "centroid_x", latlabel = "centroid_y", sourceproj = "+init=epsg:31992",  targetproj="+init=epsg:4326")
-  plotmap(min(wgscent$x)-0.002,max(wgscent$x)+0.002,min(wgscent$y)-0.007,max(wgscent$y)+0.007,wgscent)
-  return (tripdata)
+  return (list(tripdata, wgscent))
 }
 
-plotmap <- function(lon1,lon2,lat1,lat2,points,projection="+init=epsg:4326"){
+plotmap <- function(minlon,maxlon,minlat,maxlat,points,projection="+init=epsg:4326"){
   library(OpenStreetMap)
   library(ggplot2)
-  sa_map <- openmap(c(lat2, lon1), c(lat1, lon2), 
+  tol = 0
+  sa_map <- openmap(c(maxlat+tol, minlon+tol), c(minlat, maxlon), 
                     type = "opencyclemap")
   sa_map2 <- openproj(sa_map, projection = projection)
   sa_map2_plt <- OpenStreetMap::autoplot.OpenStreetMap(sa_map2) + 
         geom_point(data = points,
                aes(x = x, y = y), # slightly shift the points
-               colour = "red", size =  5) +
+               colour = "red", size =  1) +
     xlab("Longitude (°E)") + ylab("Latitude (°S)")
   plot(sa_map2_plt)
 }
--- reportes.R
+++ reportes.R
@@ -1,37 +1,60 @@
-rm(list = ls())
+#rm(list = ls())
 library(dplyr)
+library(lubridate)
 library(tictoc)
 source("libraries.R")
 
 tic()
 #1. Load data
-# data <- getData() #call once
-# data %>% filter(accuracy<1000 & activity_type!="unknown") %>% select(device_model,event,battery_level,is_moving,speed,heading,activity_type,recorded_at,latitude,longitude,company_token) ->subdata
-# save(subdata,file="subdata.R")
-load(file="subdata.R")
+data <- getData(limit=1000000) #call once
+data %>% filter(accuracy<1000 & activity_type!="unknown") %>% select(device_model,event,battery_level,is_moving,speed,heading,activity_type,recorded_at,latitude,longitude,company_token) ->subdata
+save(subdata,file="subdata.R")
+load(file="subdata.R") #800000 datos
 
 #2. Track points stats
 glimpse(subdata)
-table(subdata$is_moving, subdata$activity_type)
 boxplot(speed~activity_type,subdata)
 
-#3. Trip records stats
-subdata %>% filter(latitude>-3 & latitude < -2 & longitude > -79 & longitude < -78) -> subdata#bounding box Cuenca
+#3. Trip records stats (bounding box from Cuenca)
+minlat <- -2.9736; maxlat <- -2.7946; minlon <- -79.0911; maxlon <- -78.7716
+subdata %>% filter(latitude> minlat & latitude <  maxlat & longitude > minlon & longitude < maxlon) -> subdata#bounding box Cuenca
 datapoints <- changecoordsystem(subdata,longlabel = "longitude", latlabel = "latitude", targetproj = "+init=epsg:31992" )
-plotmap(min(datapoints$longitude)-0.002,max(datapoints$longitude)+0.002,min(datapoints$latitude)-0.007,max(datapoints$latitude)+0.007,datapoints,"+init=epsg:31992")
+plotmap(minlon,maxlon,minlat,maxlat,datapoints[1:10000,],"+init=epsg:31992")
 
+#load(file="tripdata.R")
 tripdata <- segmentTrips(datapoints, minttime = 600, maxtime = 3600) #get trips 10 minutes < x < 1 hour
+tripdata$date <- ymd_hms(tripdata$starttime) 
+tripdata$wday <- wday(tripdata$date)
+tripdata$hour <- hour(tripdata$date)
+save(tripdata,file="tripdata.R")
+prop.table(table(tripdata$wday))
+
+#stats for week day trips
+tripdata %>% filter(wday == 6 & hour>20) -> tripdata
+tripdata %>% filter(mode %in% c("on_bicycle","in_vehicle","walking")) -> tripdata
 hist(tripdata$ttime)
 table(tripdata$mode)
 boxplot(ttime ~ mode, tripdata)
-plot(tripdata$dx, tripdata$dy,main = "User destinations",xlab = "x", ylab="y",cex.axis=.95)
+hist(tripdata$tdistance)
+boxplot(tdistance ~ mode, tripdata)
 tripdata$x = tripdata$dx; tripdata$y = tripdata$dy; 
-plotmap(min(tripdata$dlong)-0.002,max(tripdata$dlong)+0.002,min(tripdata$dlat)-0.007,max(tripdata$dlat)+0.007,tripdata,"+init=epsg:31992")
-
+plotmap(minlon,maxlon,minlat,maxlat,tripdata[sample(1:nrow(tripdata),10000),],"+init=epsg:31992")
 
 #4. Encontrar zonas densas del mapa de calor
-tripdata <- getAttractors(tripdata, eps=22, minPts=10)
-save(tripdata,file="tripdata.R")
+#load(file="pois.R")
+data <- getAttractors(tripdata, eps=22, minPts=3)
+tripdata <- data[[1]]
+centroids <- data[[2]]
+plotmap(minlon,maxlon,minlat,maxlat,centroids,"+init=epsg:4326")
+save(data,file="pois.R")
 toc()
+#1142.73 sec con 800000 datos
 
+#5. Especificar atractores para una hora en particular
+tripdata %>% filter(hour == 7) -> hourlytrips
+centroids %>% filter(cluster %in% as.numeric(unique(hourlytrips$cluster))) -> hourlycentroids
+plotmap(minlon,maxlon,minlat,maxlat,hourlycentroids,"+init=epsg:4326")
+#Descartar domicilios
+#Filtrar por minimo de estudiantes
 #DESPUES: Buscar zonas atractoras de estudiantes para planificar transporte público universitario.
+#Completar viajes en base a rutinas de cada persona (viajes repertitivos que permitan ver la trayectoria)
